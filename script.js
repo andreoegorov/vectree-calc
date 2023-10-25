696aeb7b6ef62b835fb053d0228624cfdada0d0c
Andrey <andreo.egorov@mail.ru>
2023-10-25 20:22:09 +0400

    final layout

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-const backspaceButton = document.getElementById("backspace");
+const numberButtons = document.getElementsByClassName('number');
+const dotButton = document.getElementById("dot");
+
 const addButton = document.getElementById("add");
 const subButton = document.getElementById("subtract");
 const mulButton = document.getElementById("multiply");
 const divButton = document.getElementById("divide");
 const equalsButton = document.getElementById("equals");
-const clearButton = document.getElementById("clear");
-const clearEntryButton = document.getElementById("clear-entry");
-const numberButtons = document.getElementsByClassName('number');
-const dotButton = document.getElementById("dot");
+
 const percentButton = document.getElementById("percent");
-const recButton = document.getElementById("reciprocal");
 const sqrButton = document.getElementById("square-degree");
 const sqrtButton = document.getElementById("square-root"); 
 const negButton = document.getElementById("inverse-sign"); 
+
+const backspaceButton = document.getElementById("backspace");
+const clearButton = document.getElementById("clear");
+const bracketOpenButton = document.getElementById("bracket-open");
+const bracketCloseButton = document.getElementById("bracket-close");
+
+
 
