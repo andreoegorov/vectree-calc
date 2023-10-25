88d0f7903e91c206eea9cc024650ed58a7604fab
Andrey <andreo.egorov@mail.ru>
2023-10-25 20:05:21 +0400

    init

---
+++ script.js
@@ -0,0 +1,16 @@
+const backspaceButton = document.getElementById("backspace");
+const addButton = document.getElementById("add");
+const subButton = document.getElementById("subtract");
+const mulButton = document.getElementById("multiply");
+const divButton = document.getElementById("divide");
+const equalsButton = document.getElementById("equals");
+const clearButton = document.getElementById("clear");
+const clearEntryButton = document.getElementById("clear-entry");
+const numberButtons = document.getElementsByClassName('number');
+const dotButton = document.getElementById("dot");
+const percentButton = document.getElementById("percent");
+const recButton = document.getElementById("reciprocal");
+const sqrButton = document.getElementById("square-degree");
+const sqrtButton = document.getElementById("square-root"); 
+const negButton = document.getElementById("inverse-sign"); 
+
