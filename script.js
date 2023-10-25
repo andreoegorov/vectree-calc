f0a6796b2d70e53cc8eeff0dffd7905c46c7da0b
Andrey <andreo.egorov@mail.ru>
2023-10-26 00:50:49 +0400

    imp exp parsers

--- script.js
+++ script.js
@@ -18,4 +18,46 @@ const bracketOpenButton = document.getElementById("bracket-open");
 const bracketCloseButton = document.getElementById("bracket-close");
 
 
+const display = {
+    mainString:"3-2*2+6/2", //2
+    additionalString:"",
+    mainStringDOMElement:null,
+    additionalStringDOMElement:null,
+    update(){
+        display.mainStringDOMElement.innerText  = this.mainString;
+        display.additionalStringDOMElement.innerText = this.additionalString;
+    }
 
+}
+
+display.mainStringDOMElement  = document.getElementById("main-string");
+display.additionalStringDOMElement = document.getElementById("additional-string");
+display.update();
+
+equalsButton.onclick = function(){
+    display.additionalString = display.mainString + "=";
+    display.mainString = parseString( "+" ,display.mainString);
+    display.update();
+}
+
+function parseString(opr, str){
+    const reg = str.match(`\\d+\\${opr}\\d+`);
+    const string = str.replace(reg,parseExp(opr, reg[0]));
+    return string;
+}
+
+function parseExp(opr ,str){
+    const index = str.indexOf(opr);
+    return operateBinary(opr, str.slice(0,index), str.slice(index+1, str.length));
+}
+
+function operateBinary(operator ,operand1 ,operand2){
+    switch (operator){
+      case "+": return Number(operand1) + Number(operand2);
+    }
+}
+function operateUnary(operator ,operand1){
+    switch (operator){
+        case "x2": return Math.pow(operand1,2);
+      }
+}
